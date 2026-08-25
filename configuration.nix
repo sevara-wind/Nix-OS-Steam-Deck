229e8ab370c1cc5fb6ba17a7da39dd9deb98f867
sevara <sevara.wind@icloud.com>
2026-08-25 07:06:12 +0300

    Update configuration

--- configuration.nix
+++ configuration.nix
@@ -6,15 +6,38 @@
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Moscow"; 
 
-  # Принудительный сброс видеорежима UEFI для выравнивания меню по горизонтали
-  boot.loader.systemd-boot = {
-    enable = true;
-    consoleMode = "0";
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = false;
+  # Generate Russian locale files so Steam can map the OSK correctly
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "ru_RU.UTF-8/UTF-8"
+  ];
 
-  # Разворот на 90 градусов по часовой стрелке при старте ядра ОС
+  boot.loader = {
+    systemd-boot.enable = false;
+    grub.enable = false;
+    
+    limine = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+
+      style.wallpapers = [ ./wallpaper.png ];
+
+      extraConfig = ''
+        interface_rotation: 90
+        graphics: yes
+        timeout: 5
+        
+        wallpaper_style: stretch
+        background_color: #00000000
+        text_color: #ffffffff
+        text_highlight_color: #ff00ffff
+      '';
+    };
+    
+    efi.canTouchEfiVariables = true;
+  };
+
   boot.kernelParams = [ 
     "video=DSI-1:panel_orientation=right_side_up" 
     "fbcon=rotate:1" 
@@ -23,7 +46,6 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Настройки графики, звука и кнопок громкости
   jovian = {
     devices.steamdeck.enable = true;
     steamos.useSteamOSConfig = true; 
@@ -46,6 +68,8 @@
   xdg.portal.enable = true;
   services.displayManager.gdm.enable = lib.mkForce false; 
 
+  services.envfs.enable = true;
+
   users.users.sevara = {
     isNormalUser = true;
     description = "sevara";
@@ -63,7 +87,6 @@
     };
   };
 
-  # Включение Throne с системной TUN-оберткой
   programs.throne = {
      enable = true;
      tunMode.enable = true;
@@ -83,5 +106,7 @@
     util-linux
     go
     psmisc
+    appimage-run
+    steam-run
   ];
 }
